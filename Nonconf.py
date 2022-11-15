981494677419b9fe6c63074c48282f35fc3e9419
arhelyd <damitom@o2.pl>
2022-11-15 19:05:39 +0100

    Kolejny

--- Nonconf.py
+++ Nonconf.py
@@ -1,5 +1,5 @@
 print("Pierwszy feature")
-print("Drugi feature")
+print("Kolejny feature")
 
 from Punkt import Point
 punkt=Point(1,1,1)
@@ -7,5 +7,5 @@ punkt.wypisz()
 print("odległość od punktu 0,0,0:",end="\t")
 print(punkt.distance())
 from Fibo import Fibo
-print(Fibo.oblicz(10))
+print(Fibo.oblicz(12))
 print("Trzeci feature")
