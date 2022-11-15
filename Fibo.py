f779cc12c7641969793fafd239cd1d80a29e5b49
arhelyd <damitom@o2.pl>
2022-11-15 19:00:02 +0100

    Fibor

--- Fibo.py
+++ Fibo.py
@@ -1,7 +1,16 @@
 class Fibo:
 
+   # @staticmethod
+   # def oblicz(n):
+   #     if n in {0, 1}:  
+   #         return n
+   #     return Fibo.oblicz(n - 1) + Fibo.oblicz(n - 2)
+   
+   #na początku przez pomyłkę zrobiłem rekurencyjnie to teraz dodałem iteracyjnie
+    
     @staticmethod
-    def oblicz(n):
-        if n in {0, 1}:  
-            return n
-        return Fibo.oblicz(n - 1) + Fibo.oblicz(n - 2)
+    def oblicz_iter(n):
+        a,b = 0,1
+        for i in range(n):
+            a,b = b,a+b
+        return a
