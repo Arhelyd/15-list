210d52097e38e317141f5a78774fbb167589a2fa
arhelyd <damitom@o2.pl>
2022-11-15 18:37:49 +0100

    Drugi feature

--- Nonconf.py
+++ Nonconf.py
@@ -1,1 +1,9 @@
 print("Pierwszy feature")
+print("Drugi feature")
+
+from Punkt import Point
+punkt=Point(1,1,1)
+punkt.wypisz()
+print("odległość od punktu 0,0,0:",end="\t")
+print(punkt.distance())
+
